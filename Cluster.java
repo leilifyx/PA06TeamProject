706a3aaf3a52c5c38d938dd637446cfba7215c55
bliaowey <62267175+bliaowey@users.noreply.github.com>
2020-04-15 15:42:37 -0400

    Create Cluster.java

---
+++ Cluster.java
@@ -0,0 +1,15 @@
+package pa06;
+import java.util.ArrayList;
+
+/**
+ * A cluster is a cluster point (which is itself a sample)
+ * and a list of Samples (the one's closest to that sample point, ideally).
+ * @author presenting
+ *
+ */
+public class Cluster {
+
+  Sample s;
+  ArrayList<Sample> c;
+
+}
